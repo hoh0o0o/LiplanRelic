e567830edcf6e3f70e7201cd56b8aa3515eaf19c
西村はるほ <s276183@wakayama-u.ac.jp>
2024-10-18 10:50:17 +0900

    最初の完成版

---
+++ public/fadeout.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+  function fadeOutAndRedirect() {
+      let body = document.body;
+      body.style.transition = "opacity 1s";
+      body.style.opacity = 0;
+
+      setTimeout(function() {
+          window.location.href = "./home.html";
+      }, 1000); //1秒で遷移しな！
+  }
+
+  setTimeout(function() {
+      fadeOutAndRedirect();
+  }, 3000); //3秒間待ってやる
+});
